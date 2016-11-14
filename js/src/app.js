d5cfa3b88acf5fd941110c4f2d1aebeabab86417
Ondrej Zara <ondrej.zara@gmail.com>
2016-11-14 22:53:49 +0100

    fix

--- js/src/app.js
+++ js/src/app.js
@@ -112,7 +112,7 @@ platform.onOpen(processArgs);
 if (platform.argv.length) {
 	processArgs(platform.argv, platform.baseURI);
 } else {
-	alert("No arguments received, starting in dummy mode. Re-launch with more arguments, drop some files or control a running instance to play something.");
+	alert("No arguments received, starting in dummy mode. Re-launch with more arguments, drop some files, or control a running instance to play something.");
 }
 
 window.addEventListener("dragover", e => {
